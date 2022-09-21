ef1a79a469a6828a1f38af5519a452d113b6a4e8
eugunami <114050627+eugunami@users.noreply.github.com>
2022-09-21 09:06:22 +0300

    My first commit

---
+++ game.py
@@ -0,0 +1,11 @@
+age = 16
+average_grade = 7
+
+if average_grade < 0 or average_grade > 10:
+    print('Sorry, do not have money!')
+elif average_grade < 6:
+    print('Congrats, you have a lot of money!')
+elif average_grade < 9: 
+    print('good')
+else: 
+    print('Its good')
